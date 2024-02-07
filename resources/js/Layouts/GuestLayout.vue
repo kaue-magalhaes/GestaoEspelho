20077214d00ebe8b23ac4c1c43c1dbeb3c760faf
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-07 11:40:44 -0300

    Refactor GuestLayout.vue to use CardHeader and CardContent components

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,19 +1,18 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
-import { Card } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 </script>
 
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
-
         <Card class="w-full sm:max-w-md mt-6 p-6">
-            <slot />
+            <CardHeader>
+                <slot name="card-header"/>
+            </CardHeader>
+            <CardContent>
+                <slot />
+            </CardContent>
         </Card>
     </div>
 </template>
