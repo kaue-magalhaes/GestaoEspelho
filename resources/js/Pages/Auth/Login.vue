cfbea530c34219b2907f36051f3464ddaebd0227
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-07 11:51:16 -0300

    Update login page text

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -77,11 +77,11 @@ const submit = () => {
                     :href="route('password.request')"
                     class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                    Forgot your password?
+                    Esqueceu sua senha?
                 </Link>
 
                 <Button class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
+                    Login
                 </Button>
             </div>
         </form>
