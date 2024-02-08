bcecee1ac8455a0664af028abbba44464440c7c0
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-08 09:40:49 -0300

    Update Espelho.vue with new card title

--- resources/js/Pages/Espelho.vue
+++ resources/js/Pages/Espelho.vue
@@ -11,7 +11,9 @@ import { Card, CardHeader, CardTitle } from '@/Components/ui/card';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <Card>
                     <CardHeader>
-                        <CardTitle class="text-lg p-4 font-semibold">Espelho</CardTitle>
+                        <CardTitle class="text-lg p-4 font-semibold">
+                            Área de criação do espelho
+                        </CardTitle>
                     </CardHeader>
                 </Card>
             </div>
