0e161b9d9eed00236d5d9110a8ff4c0e8ba7c1ae
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-08 12:23:36 -0300

    Refactor Espelho.vue component and add Carousel and Preview components

--- resources/js/Pages/Preview.vue
+++ resources/js/Pages/Preview.vue
@@ -5,66 +5,59 @@ import { Card, CardHeader } from '@/Components/ui/card';
 </script>
 
 <template>
-    <Head title="Preview" />
-    <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-full mx-auto sm:px-6 lg:px-8">
-                <Card>
-                    <CardHeader>
-                        <div class="flex justify-center items-center">
-                            <div class="flex flex-col items-center">
-                                <div class="flex flex-col items-center">
-                                    <a href="https://www.mpap.mp.br" target="_blank" class="flex flex-col items-center">
-                                        <div class="h-16 w-auto sm:h-20">
-                                            <img
-                                            class="h-16 w-auto sm:h-20" 
-                                            src="https://www.mpap.mp.br/templates/portal/images/logo-mpap.png"
-                                            >
-                                        </div>
-                                        <h1 class="font-bold text-gray-700 dark:text-gray-200">
-                                            Procuradoria Geral de Justiça
-                                        </h1>
-                                    </a>
-
-                                    <div class="text-sm text-gray-500 dark:text-gray-400">
-                                        Procuradorias de Justiça do Estado do Amapá
-                                    </div>
-                                    <div class="text-sm text-gray-500 dark:text-gray-400">
-                                        Espelho de 07/02/2024 a 15/02/2024
-                                    </div>
-
-                                    <div>
-                                        <!-- Pequena tabela sobre o PLANTÃO DE MACAPÁ PARA ATENDIMENTOS EM CARÁTER DE URGÊNCIA -->
-                                    </div>
-
-                                    <div>
-                                        <!-- Tabela das Procuradorias de Justiça de Entrância Final - Macapá -->
-                                    </div>
-
-                                    <div>
-                                        <!-- Tabela das Procuradorias de Justiça de Entrância Final - Macapá (Especializadas) -->
-                                    </div>
-
-                                    <div>
-                                        <!-- Pequena tabela sobre o PLANTÃO DE MACAPÁ PARA ATENDIMENTOS EM CARÁTER DE URGÊNCIA (Novamente) -->
-                                    </div>
-                                    <div>
-                                        <!-- Tabela das Procuradorias de Justiça de Entrância Final - Santana -->
-                                    </div>
-
-                                    <div>
-                                        <!-- Tabela das Procuradorias de Justiça de Entrância Inicial - Interiores -->
-                                    </div>
-
-                                    <div>
-                                        <!-- Promotores de Justiça Substitutos -->
-                                    </div>
-                                </div>
+    <Card>
+        <CardHeader>
+            <div class="flex justify-center items-center">
+                <div class="flex flex-col items-center">
+                    <div class="flex flex-col items-center">
+                        <a href="https://www.mpap.mp.br" target="_blank" class="flex flex-col items-center">
+                            <div class="h-16 w-auto sm:h-20">
+                                <img
+                                class="h-16 w-auto sm:h-20" 
+                                src="https://www.mpap.mp.br/templates/portal/images/logo-mpap.png"
+                                >
                             </div>
+                            <h1 class="font-bold text-gray-700 dark:text-gray-200">
+                                Procuradoria Geral de Justiça
+                            </h1>
+                        </a>
+
+                        <div class="text-sm text-gray-500 dark:text-gray-400">
+                            Procuradorias de Justiça do Estado do Amapá
                         </div>
-                    </CardHeader>
-                </Card>
+                        <div class="text-sm text-gray-500 dark:text-gray-400">
+                            Espelho de 07/02/2024 a 15/02/2024
+                        </div>
+
+                        <div>
+                            <!-- Pequena tabela sobre o PLANTÃO DE MACAPÁ PARA ATENDIMENTOS EM CARÁTER DE URGÊNCIA -->
+                        </div>
+
+                        <div>
+                            <!-- Tabela das Procuradorias de Justiça de Entrância Final - Macapá -->
+                        </div>
+
+                        <div>
+                            <!-- Tabela das Procuradorias de Justiça de Entrância Final - Macapá (Especializadas) -->
+                        </div>
+
+                        <div>
+                            <!-- Pequena tabela sobre o PLANTÃO DE MACAPÁ PARA ATENDIMENTOS EM CARÁTER DE URGÊNCIA (Novamente) -->
+                        </div>
+                        <div>
+                            <!-- Tabela das Procuradorias de Justiça de Entrância Final - Santana -->
+                        </div>
+
+                        <div>
+                            <!-- Tabela das Procuradorias de Justiça de Entrância Inicial - Interiores -->
+                        </div>
+
+                        <div>
+                            <!-- Promotores de Justiça Substitutos -->
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </AuthenticatedLayout>
+        </CardHeader>
+    </Card>
 </template>
