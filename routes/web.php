f61ddc5a4ec067c4474d119228a0cec4e345f46e
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-08 11:26:21 -0300

    Add preview route to web.php

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/preview' , function () {
+        return Inertia::render('Preview');
+    })->name('preview');
 });
 
 Route::get('/test', function () {
