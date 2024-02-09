d9bdbbef4e6492ba96d95be1baed82acfb7c9561
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-09 10:48:49 -0300

    Refactor DocumentController create method

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -14,18 +14,7 @@ class DocumentController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function create(Request $request)
     {
         //
     }
