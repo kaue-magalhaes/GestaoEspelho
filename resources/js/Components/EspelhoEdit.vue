19ef21cc702dfba529032c0aa06f1352b74252fd
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-16 12:38:34 -0300

    Adicionado componente EspelhoEdit.vue e atualizado componente Espelho.vue

---
+++ resources/js/Components/EspelhoEdit.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/Components/ui/label';
+import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card';
+import Calendar from 'primevue/calendar';
+
+const dates = ref(null);
+
+</script>
+
+<template>
+  <Card>
+    <div class="max-w-5xl mx-auto">
+      <CardHeader class="flex flex-row justify-between">
+        <CardTitle class="font-semibold">
+          Espelho do Ministério Público
+        </CardTitle>
+        <span class="flex items-end">
+          <Label for="period" class="text-base mr-4">
+            Período:
+          </Label>
+          <Calendar
+            selectionMode="range"
+            dateFormat="dd/mm/yy"
+            inputId="period"
+            showIcon
+            placeholder="Selecione o período"
+            v-model=dates
+            class="text-sm"
+          />
+        </span>
+      </CardHeader>
+  
+      <CardContent>
+        <div class="mt-8 flex flex-col space-y-4">
+          <div class="grid grid-cols-2 gap-4">
+            <Card class="col-span-2 flex flex-col py-4 items-center space-y-2 w-full">
+              <Label class="text-xl">
+                Plantão de Atendimentos em Caráter de Urgência - Macapá:
+              </Label>
+              <Button variant="ghost" class="w-1/4">
+                Adicionar +
+              </Button>
+            </Card>
+          </div>
+        </div>
+      </CardContent>
+    </div>
+  </Card>
+</template>
--- resources/js/Pages/Espelho.vue
+++ resources/js/Pages/Espelho.vue
@@ -1,45 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { ref, watch } from 'vue';
-import { Head, useForm } from '@inertiajs/vue3';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/Components/ui/input';
+import { Head } from '@inertiajs/vue3';
 import { Carousel, CarouselContent, CarouselItem } from '@/Components/ui/carousel';
-import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card';
-
-import Calendar from 'primevue/calendar';
 import Preview from './Preview.vue';
+import EspelhoEdit from '@/Components/EspelhoEdit.vue';
 
-const rows = ref(1);
-const columns = ref(1);
-const tableData = ref([['']]);
-
-const dates = ref(null);
-
-const form = useForm({
-    dates: null,
-});
-
-watch(dates, (newValue) => {
-    form.dates = newValue;
-    submit();
-});
-
-const submit = () => {
-    form.post(route('espelho.create'));
-};
-
-const addRow = () => {
-    rows.value++;
-    tableData.value.push(Array(columns.value).fill(''));
-};
-
-const addColumn = () => {
-    columns.value++;
-    tableData.value.forEach((row, index) => {
-        tableData.value[index].push('');
-    });
-};
 </script>
 
 <template>
@@ -50,44 +15,9 @@ const addColumn = () => {
                 <Carousel class="focus-visible:outline-none">
                     <CarouselContent>
                         <CarouselItem>
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle class="text-lg p-4 font-semibold">
-                                        Área de criação do espelho
-
-                                    </CardTitle>
-
-                                    <div class="flex justify-end items-center p-4">
-                                        <Button @click="addRow">Adicionar linha</Button>
-                                        <Button class="ml-2" @click="addColumn">Adicionar coluna</Button>
-                                    </div>
-                                </CardHeader>
-
-                                <CardContent>
-                                    <div class="flex justify-center mb-2">
-                                        <Calendar v-model="dates" selectionMode="range" :manualInput="false" />
-                                    </div>
-                                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                            <tr>
-                                                <th v-for="column in columns" :key="column" class="px-6 py-3">
-                                                    Coluna {{ column }}
-                                                </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr v-for="(row, rowIndex) in tableData" :key="rowIndex" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                                <td v-for="(cell, columnIndex) in row" :key="columnIndex" class="px-6 py-4">
-                                                    <Input v-model="tableData[rowIndex][columnIndex]" />
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </CardContent>
-                            </Card>
+                            <EspelhoEdit />
                         </CarouselItem>
                         <CarouselItem>
-                            <VuePdfEmbed annotation-layer text-layer :source="pdfSource" />
                             <Preview />
                         </CarouselItem>
                     </CarouselContent>
