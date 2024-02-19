27cc19aefe5e4f3c0195b7dfe4107b4d03c8a064
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-19 12:37:28 -0300

    Adiciona modelo Promotor ao compartilhar dados no middleware HandleInertiaRequests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Promotor;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -33,6 +34,9 @@ class HandleInertiaRequests extends Middleware
             ...parent::share($request),
             'auth' => [
                 'user' => $request->user(),
+            ],
+            'promotores' => [
+                'all' => Promotor::all(),
             ],
         ];
     }
