0f8de6c402be01204aa10b182ad8ec68a89dbd8e
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-19 12:37:22 -0300

    Adiciona model, factory, migration e seeder para Promotor

---
+++ database/seeders/PromotorSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Promotor;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PromotorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Promotor::factory()
+            ->count(10)
+            ->create();
+    }
+}
