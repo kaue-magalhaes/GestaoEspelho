e7f3bb7b3794c38019eb4f9bbb816cee1bafba76
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-19 12:38:50 -0300

    Remover importação desnecessária de WithoutModelEvents em PromotorSeeder

--- database/seeders/PromotorSeeder.php
+++ database/seeders/PromotorSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Promotor;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PromotorSeeder extends Seeder
