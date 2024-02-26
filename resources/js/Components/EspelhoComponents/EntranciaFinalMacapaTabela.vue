91a7dcbf981e29349009930a1e35c0710d7a856a
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-02-26 07:51:50 -0300

    Adiciona o componente de tabela de Entrância Final Macapá

---
+++ resources/js/Components/EspelhoComponents/EntranciaFinalMacapaTabela.vue
@@ -0,0 +1,147 @@
+<script setup lang="ts">
+import { Promotoria, Promotor } from '@/types';
+
+import { Input } from '@/Components/ui/input';
+import { Label } from '@/Components/ui/label';
+import { Button } from '@/Components/ui/button';
+import DatePicker from '@/Components/DatePicker.vue';
+import { Dialog, DialogContent, DialogTrigger, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from '@/Components/ui/dialog';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue} from '@/Components/ui/select'
+
+const props = defineProps({
+  promotorias: {
+    type: Object as () => { all: Promotoria[]; },
+    required: true,
+  },
+  promotores: {
+    type: Object as () => { all: Promotor[]; },
+    required: true,
+  },
+});
+
+const eventos = [
+  { id: 1, nome: 'Férias', selecionado: false },
+  { id: 2, nome: 'Licença', selecionado: false },
+  { id: 3, nome: 'Plantão', selecionado: false },
+  { id: 4, nome: 'Reunião', selecionado: true },
+  { id: 5, nome: 'Outros', selecionado: false },
+];
+</script>
+
+<template>
+  <!-- Tabela das Procuradorias de Justiça de Entrância Final - Macapá -->
+  <table class="w-full text-black">
+    <thead class="text-xs text-black uppercase bg-gray-300 border-x border-t">
+      <tr class="text-center text-base">
+        <th class="px-6 py-4">
+          PROMOTORIAS DE MACAPÁ
+        </th>
+        <th class="px-6 py-3">
+          Promotor(a)
+        </th>
+        <th class="px-6 py-3">
+          Período
+        </th>
+      </tr>
+    </thead>
+    <tbody class="text-start">
+      <tr class="bg-white hover:bg-gray-50 border" v-for="promotoria in props.promotorias.all" :key="promotoria.id">
+        <th class="px-6 py-4 font-medium whitespace-nowrap">
+          <div class="flex flex-col items-start">
+            <h1 class="font-bold text-gray-900 dark:text-gray-200">
+              {{ promotoria.nome }}
+            </h1>
+          </div>
+        </th>
+        <td class="px-6 py-4">
+          <h1 class="font-bold text-gray-900 dark:text-gray-200">
+            {{ promotoria.promotor.nome }}
+          </h1>
+        </td>
+        <td class="px-6 py-4">
+          <Dialog>
+            <DialogTrigger as-child>
+              <Button variant="link">
+                Adicionar Evento
+              </Button>
+            </DialogTrigger>
+            <DialogContent class="w-full max-w-2xl">
+              <DialogHeader>
+                <DialogTitle>
+                  Adicionar Evento
+                </DialogTitle>
+                <DialogDescription>
+                  Adicione um evento para o Promotor de Justiça {{ promotoria.promotor.nome }}.
+                </DialogDescription>
+              </DialogHeader>
+              <div class="flex flex-col space-y-4">
+                <div class="flex items-center space-x-4">
+                  <div class="w-1/2">
+                    <Label class="text-base whitespace-nowrap">
+                      Tipo do Evento:
+                    </Label>
+                    <Select class="mb-2 w-full">
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione o tipo de evento" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Eventos</SelectLabel>
+                          <SelectItem v-for="evento in eventos" :key="evento.id" :value="evento.nome">
+                            {{ evento.nome }}
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div class="w-1/2">
+                    <Label class="text-base">
+                      Período:
+                    </Label>
+                    <DatePicker />
+                  </div>
+                </div>
+                <div class="flex items-center space-x-4">
+                  <div class="w-1/2">
+                    <Label class="text-base">
+                      Título do Evento:
+                    </Label>
+                    <Input type="text" placeholder="Digite o título do evento" />
+                  </div>
+                  <div class="w-1/2">
+                    <Label class="text-base whitespace-nowrap">
+                      Promotor designado:
+                    </Label>
+                    <Select class="mb-2 w-full">
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione o Promotor que foi designado" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Promotores</SelectLabel>
+                          <SelectItem v-for="promotor in props.promotores.all" :key="promotor.id" :value="promotor.nome">
+                            {{ promotor.nome }}
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </div>
+              <DialogFooter>
+                <Button variant="default">
+                  Adicionar Evento
+                </Button>
+                <DialogClose as-child>
+                  <Button variant="secondary">
+                    Cancelar
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
