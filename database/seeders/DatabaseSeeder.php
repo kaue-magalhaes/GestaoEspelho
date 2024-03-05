1d6dc2176408d7cfcb8eb8cad83529b3817f0445
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-03-05 13:14:33 -0300

    Adiciona seeders Espelho, Evento e UrgenciaAtendimento

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Console\Scheduling\Event;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            EspelhoSeeder::class,
             PromotorSeeder::class,
             PromotoriaSeeder::class,
+            EventoSeeder::class,
+            UrgenciaAtendimentoSeeder::class,
             UserSeeder::class,
         ]);
     }
