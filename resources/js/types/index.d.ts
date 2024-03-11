152d1b7185e6e932c92dd3ab01ec258cca1d3781
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-03-11 09:57:37 -0300

    Atualiza tipos de UrgenciaAtendimento

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -44,7 +44,14 @@ export interface Evento {
     promotor_designado_id: string;
 }
 
-export interface UrgenciaAtendimento {
+export interface UrgenciaAtendimentoClientSide {
+    uuid: string;
+    periodo_inicio: string;
+    periodo_fim: string;
+    promotor_designado_id: string;
+}
+
+export interface UrgenciaAtendimentoServeSide {
     id: string;
     periodo_inicio: string;
     periodo_fim: string;
