967f3ed62170856ba06aecc968864de97c77f32c
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-03-14 18:53:27 -0300

    Adiciona novas Historico models vazias

---
+++ app/Models/Historico/HistoricoEvento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Historico;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HistoricoEvento extends Model
+{
+    use HasFactory;
+}
