8f025dd87a3b705095251d5982c8cf5ed3f1b318
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-04-01 08:14:02 -0300

    Atualiza rota para exigir autenticação

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', [EspelhoController::class, 'editor'])
+Route::middleware('auth')->group(function () {
+    Route::get('/', [EspelhoController::class, 'editor'])
     ->name('espelho.editor');
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
