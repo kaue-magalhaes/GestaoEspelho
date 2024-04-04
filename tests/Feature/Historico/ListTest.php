415547a8e12be62dd5aeee77676f9ece99998974
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-04-04 11:21:32 -0300

    DSIS-1974 add the Historico listing test

---
+++ tests/Feature/Historico/ListTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Historico\HistoricoEspelho;
+use App\Models\User;
+
+use function Pest\Laravel\get;
+
+it('should be able to view the Historicos list', function () {
+    $user       = User::factory()->create();
+    $historicos = HistoricoEspelho::factory()->count(5)->create([
+        'usuario_id' => $user->id,
+    ]);
+    $this->actingAs($user);
+
+    $request = get(route('espelho.history'));
+
+    foreach ($historicos as $historico) {
+        $request->assertSee($historico->created_at->toJSON());
+    }
+});
