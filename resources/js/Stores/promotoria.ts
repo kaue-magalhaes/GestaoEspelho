8c93d073173d6f263ce64b2b5c379ee77d6bc1e6
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-05-03 13:00:41 -0300

    DSIS-2036 adicionando mensagem de sucesso ao editar uma promotoria

--- resources/js/Stores/promotoria.ts
+++ resources/js/Stores/promotoria.ts
@@ -6,7 +6,14 @@ import {toast} from "vue-sonner";
 export const usePromotoriaStore = defineStore('promotoria', () => {
 
     function editPromotoria(id: string, request: any) {
-        router.put(route('promotoria.update', id), request);
+        router.put(route('promotoria.update', id), request, {
+            onFinish: () => {
+                const successMessage = usePage().props.flash.success;
+                if (successMessage) {
+                    toast.success(`${successMessage}`);
+                }
+            },
+        });
     }
 
     function deletePromotoria(id: string, nome: string) {
