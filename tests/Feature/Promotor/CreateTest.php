4ddc029e48a777eb6cd01708ccdbf6ec7ed25aff
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-05-16 12:41:17 -0300

    DSIS-2036 feat: Implement validation to prevent creation of Promotor without a name
    Description:
    Implemented validation to prevent the creation of a Promotor without specifying a name. This validation ensures that the name field is required when creating a new Promotor, helping to maintain data integrity and completeness.
    
    Added Test:
    - Created a test to verify that it is not possible to create a Promotor without providing a name. The test sets up a scenario with a logged-in user and simulates a POST request to the create Promotor endpoint without including the name field. It asserts that the response contains errors for the name field in the session and that no Promotor record is created in the database.

--- tests/Feature/Promotor/CreateTest.php
+++ tests/Feature/Promotor/CreateTest.php
@@ -3,6 +3,7 @@
 use App\Models\InternalSystemUser;
 
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\post;
 
@@ -51,7 +52,7 @@ it('should only be able to create a Promotor if is authorized', function () {
     ]);
 
     $request->assertForbidden();
-    \Pest\Laravel\assertDatabaseCount('promotores', 0);
+    assertDatabaseCount('promotores', 0);
 
     actingAs($userAuthorized);
     $request = post(route('promotor.store'), [
@@ -66,3 +67,24 @@ it('should only be able to create a Promotor if is authorized', function () {
         'is_substituto' => false,
     ]);
 });
+
+it('should not be possible to create a Promotor without a name', function () {
+    $user = InternalSystemUser::factory()->create();
+    $user->niveis()->create([
+        'sistema' => 'Sol',
+        'nivel'   => fake()->numberBetween(5, 10),
+        'status ' => 1,
+    ]);
+
+    actingAs($user);
+    $request = post(route('promotor.store'), [
+        'is_substituto' => false,
+    ]);
+
+    $request->assertSessionHasErrors('nome');
+    assertDatabaseCount('promotores', 0);
+});
+
+it('should not be possible to create a Promotor without a in_substituto value', function () {
+
+})->todo();
